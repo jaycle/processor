5db359c4d5538a5f70f81b2662eb334ffddb4fc9
jaycle <jayson.cleary@gmail.com>
2015-04-15 21:21:00 -0400

    Simplifies syntax

--- WriteBack.vhd
+++ WriteBack.vhd
@@ -16,32 +16,14 @@ entity WriteBack is
 end WriteBack;
 
 architecture arch of WriteBack is
-  
-  component Mux 
-  Port(   selectL : in std_logic;
-          Min1 : in std_logic_vector(7 downto 0); --Select if SelectL is low
-          Min2 : in std_logic_vector(7 downto 0); --Select if SelectL is High
-          muxout : out std_logic_vector(7 downto 0)
-          );
- end component;
- 
- component DMEM is
-    generic(N:integer); 
-    port(
-       ALUop :in std_logic_vector(3 downto 0);
-       Writedata :in std_logic_vector(N-1 downto 0);
-       location_address : in std_logic_vector(N-1 downto 0);
-       data_out :out std_logic_vector(N-1 downto 0));
-    end component;
-  constant N: integer:=8;
+
+constant N: integer:=8;
 
 signal memD : std_logic_vector(N-1 downto 0);
-for all: DMEM use entity work.DMEM(arch);
-for all: mux use entity work.mux_2_1(behavioral);
 
 begin 
   
-portmap : DMEM
+Data : entity work.DMEM(arch)
   generic map (N=>N)
   port map (
            ALUop =>ALUop,
@@ -49,12 +31,12 @@ portmap : DMEM
            location_address =>location_Address,
            data_out => memD);
        
-portMUX : Mux
+mux : entity work.mux_2_1(behav)
   Port Map(   
           selectL => ALU_op_mux_e, 
           Min1 => ALU_Answer, --select if low
           Min2 => memD, --select if high
           muxout => wr_Bk
           );
-          
+
 end arch;
