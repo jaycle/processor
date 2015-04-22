8a15c4cfedbe181fe9b1a06de1df0ea8653f9aa3
jaycle <jayson.cleary@gmail.com>
2015-04-21 20:56:54 -0400

    Adds nop instruction at beginning

--- IMEM.vhd
+++ IMEM.vhd
@@ -42,41 +42,42 @@ constant JZ : std_logic_vector(7 downto 0) := "11010000";
 constant JNZ : std_logic_vector(7 downto 0) := "11100000"; 
 constant RETI : std_logic_vector(7 downto 0) := "11110000"; 
 
-TYPE data IS ARRAY((depth)-1 DOWNTO 0) OF std_logic_vector(width-1 DOWNTO 0);
+TYPE data IS ARRAY(0 to (depth-1)) OF std_logic_vector(width-1 DOWNTO 0);
 SIGNAL mem: data;
   
 BEGIN
     -- Manually filled instructions
 --            INST - Rx    - Ry/Im
-	MEM(0) <= ADDI 	& "0000" & "00001111";
-	MEM(1) <= ADDI 	& "0001" & "00110011";
-	MEM(2) <= ADD  	& "0010" & "0001";
-	MEM(3) <= SUB  	& "0011" & "0010";
-	MEM(4) <= INC  	& "0011"	& "0000"; -- Ry not used
-	MEM(5) <= DEC  	& "0011" & "0000"; -- Ry not used
-	MEM(6) <= SET  	& "0100" & "0000";
-	MEM(7) <= SHL  	& "0100" & "0001"; 
-	MEM(8) <= SHR  	& "0100" & "0001";
-	MEM(9) <= LNOT 	& "0000" & "0000";
-	MEM(10) <=LNOR 	& "0001" & "0000";
-	MEM(11) <=LNAND	& "0001" & "0000";
-	MEM(12) <=LXOR 	& "0001" & "0000";
-	MEM(13) <=LAND 	& "0001" & "0000";
-	MEM(14) <=LOR  	& "0001" & "0000";
-	MEM(15) <=CLR  	& "0100" & "0000";  -- clear reg for slt instr
-	MEM(16) <=SET  	& "0101" & "0000";  -- set reg for slt instr
-	MEM(17) <=SLT 	& "0100" & "0101";  -- Rx should go from zero to 1
-	MEM(18) <=MOV  	& "0100" & "1111";  -- Last Rx to reg15
-	MEM(19) <=EI	& "0000" & "1111";  -- enables all 4 interupts
-	MEM(20) <= STIN & "1111" & "0000";  -- store Value at Ry into MEM[1]
-	MEM(21) <= LDIN & "1110" & "1111";  -- load back Value into R14
-	MEM(22) <= STR	& "0000" & "00000010" ; -- store in MEM[2]
-	MEM(23) <= LDR	& "0001" & "00000010" ; -- load from MEM[2]
-	MEM(24) <= JMP  		 & "00011011" ; -- JUMP to IMEM[27]
-	MEM(25) <= NOP;  -- should be skipped
-	MEM(26) <= NOP;  -- ditto  
-	MEM(27) <= NOP;  -- gets executed
-	MEM(28) <= CLR  & "0000" & "0000";
+	MEM(0) <= NOP;  -- first instruction
+	MEM(1) <= ADDI 	& "0000" & "00001111";
+	MEM(2) <= ADDI 	& "0001" & "00110011";
+	MEM(3) <= ADD  	& "0010" & "0001";
+	MEM(4) <= SUB  	& "0011" & "0010";
+	MEM(5) <= INC  	& "0011"	& "0000"; -- Ry not used
+	MEM(6) <= DEC  	& "0011" & "0000"; -- Ry not used
+	MEM(7) <= SET  	& "0100" & "0000";
+	MEM(8) <= SHL  	& "0100" & "0001"; 
+	MEM(9) <= SHR  	& "0100" & "0001";
+	MEM(10) <= LNOT 	& "0000" & "0000";
+	MEM(11) <=LNOR 	& "0001" & "0000";
+	MEM(12) <=LNAND	& "0001" & "0000";
+	MEM(13) <=LXOR 	& "0001" & "0000";
+	MEM(14) <=LAND 	& "0001" & "0000";
+	MEM(15) <=LOR  	& "0001" & "0000";
+	MEM(16) <=CLR  	& "0100" & "0000";  -- clear reg for slt instr
+	MEM(17) <=SET  	& "0101" & "0000";  -- set reg for slt instr
+	MEM(18) <=SLT 	& "0100" & "0101";  -- Rx should go from zero to 1
+	MEM(19) <=MOV  	& "0100" & "1111";  -- Last Rx to reg15
+	MEM(20) <=EI	& "0000" & "1111";  -- enables all 4 interupts
+	MEM(21) <= STIN & "1111" & "0000";  -- store Value at Ry into MEM[1]
+	MEM(22) <= LDIN & "1110" & "1111";  -- load back Value into R14
+	MEM(23) <= STR	& "0000" & "00000010" ; -- store in MEM[2]
+	MEM(24) <= LDR	& "0001" & "00000010" ; -- load from MEM[2]
+	MEM(25) <= JMP  		 & "00011011" ; -- JUMP to IMEM[27]
+	MEM(26) <= NOP;  -- should be skipped
+	MEM(27) <= NOP;  -- ditto  
+	MEM(28) <= NOP;  -- gets executed
+	MEM(29) <= CLR  & "0000" & "0000";
    -- ask about JZ and JNZ commands  
   Output: process(addr)
   begin
