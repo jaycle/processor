a6252517b48da32e82dceea6f265bd21da06a302
jaycle <jayson.cleary@gmail.com>
2015-04-23 14:24:21 -0400

    Clock dmem write

--- DMEM.vhd
+++ DMEM.vhd
@@ -5,6 +5,7 @@ use IEEE.Numeric_Std.all;
 entity DMEM is
   generic(N:integer := 8); 
   port(
+	   clk : in std_logic;
        ALUop :in std_logic_vector(3 downto 0);
        writeData :in std_logic_vector(N-1 downto 0);
        location_address : in std_logic_vector(N-1 downto 0);
@@ -19,12 +20,15 @@ type memory is array (0 to (2**N)-1) of std_logic_vector(N-1 downto 0);
 
 BEGIN
   
-process(ALUop, writeData,Location_address) is
+process(clk, ALUop, writeData,Location_address) is
 
   begin
-      if ALUop = "1001" or ALUop ="1011" then  --Store
+	if (falling_edge(clk)) then
+      if ALUop = "1001" or ALUop ="1011" then  --Store on 0x9 or 0xB
         mem(to_integer(unsigned(location_address))) <= WriteData;
-    elsif(ALUop="1000" or ALUop = "1010") then --Load
+	  end if;
+
+    elsif(ALUop="1000" or ALUop = "1010") then --Load on 0x8 or 0xA
        data_out <= mem(to_integer(unsigned(location_address))); 
 	else
 		data_out <= (others => '0');   
