72ae90846b1383bddc5f0f5b96cc72ca92dfc1a3
tmcgukin <tmcgukin@uncc.edu>
2015-04-23 11:40:39 -0400

    added delay for Wr signal

--- Decode_Execute.vhd
+++ Decode_Execute.vhd
@@ -14,20 +14,31 @@ ENTITY Decode_Execute IS
 END Decode_Execute;
 
 ARCHITECTURE arch OF Decode_Execute IS
+signal w_en_inter : std_logic;
+signal wr_inter : std_logic_vector(3 downto 0);
+
 BEGIN
-  PROCESS(Clk)
+  single : PROCESS(Clk)
   BEGIN
     IF rising_edge(clk) THEN
 	  rdx <= rdx_in; 
 	  rdy <= rdy_in; 
-	  wr <= wr_in; 
+	  wr_inter <= wr_in; 
 	  alu_op <= alu_op_in; 
 	  alu_sel <= alu_sel_in; 
 	  imdata <= imdata_in; 
 	  ry_im <= ry_im_in; 
 	  sel_dmem <= sel_dmem_in; 
 	  wb_sel <= wb_sel_in; 
-	  w_en <= w_en_in; 
+	  w_en_inter <= w_en_in; 
     END IF;
   END PROCESS;
+
+  double : process (clk)
+  begin
+     If rising_edge(clk) then
+	  w_en <= w_en_inter;
+	  wr <= wr_inter;
+     end if;
+     end process;
 END ARCHITECTURE;
